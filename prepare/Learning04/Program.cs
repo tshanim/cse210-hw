79d0fc0daedc4d54090f198891abd43a9710f20e
William Tshani <williamtshani@gmail.com>
2024-10-16 14:19:27 +0200

    Completed Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -10,7 +10,7 @@ class Program
 
         // Now create the derived class assignments
         MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
-        Console.WriteLine(a2.GetHomeorkList());
+        Console.WriteLine(a2.GetHomeWorkList());
 
         WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The causes of World War II");
         Console.WriteLine(a3.GetSummary());
