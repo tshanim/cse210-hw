ff89ec291508473d0fade48f93e32b94765ae501
William Tshani <williamtshani@gmail.com>
2024-10-30 09:22:44 +0200

    Completed Foundation 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Exercise> exerciseList = new List<Exercise>();
+
+        Running running = new Running("July 15, 2023", 30, 4.8);
+        exerciseList.Add(running);
+
+        StationaryBicycles stationaryBicycles = new StationaryBicycles("July 17, 2023", 60, 15);
+        exerciseList.Add(stationaryBicycles);
+
+        Swimming swimming = new Swimming("July 24, 2023", 20, 20);
+        exerciseList.Add(swimming);
+
+        Console.WriteLine("Exercise:");
+        Console.WriteLine();
+
+        foreach (Exercise exercise in exerciseList)
+        {
+            Console.WriteLine(exercise.GetSummary());
+        }
     }
 }
